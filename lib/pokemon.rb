917046ce590074dfd8c187a50f656831bcee4be0
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-03 21:03:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -31,7 +31,7 @@ class Pokemon
       SELECT * FROM pokemon 
       WHERE id = ? 
     SQL
-    db.execute(sql, id).collect {|row| self.new(id:row[0], name:row[1], type:row[2], db)}
+    db.execute(sql, id).collect {|row| self.new(id:row[0], name:row[1], type:row[2], db:db)}
   end 
   
   def update
